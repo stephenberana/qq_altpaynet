b9942c4952c747b9bac29f3f958ac7bfadfc5168
stephenberana <stephenberana@gmail.com>
2022-04-28 14:57:28 +0800

    "Adds admin and customer screens"

--- src/main/java/com/mycompany/javaactivitybootcamp/JavaActivityBootcamp.java
+++ src/main/java/com/mycompany/javaactivitybootcamp/JavaActivityBootcamp.java
@@ -6,6 +6,8 @@
 package com.mycompany.javaactivitybootcamp;
 
 import com.mycompany.javaactivitybootcamp.model.User;
+import java.util.ArrayList;
+import java.util.List;
 //import java.awt.TextField;
 import java.util.Scanner;
 
@@ -19,10 +21,14 @@ public class JavaActivityBootcamp {
     private static Scanner scanner = new Scanner(System.in);
     
     public static void main(String[] args) {
-        System.out.println("Welcome to my shop! \n"
-                            + "1 - Login \n" 
-                            + "0 - Exit \n");
-        System.out.print("What do you want to do? : ");
+        List<User> users = new ArrayList<>();
+                        System.out.println("***********************");
+                        System.out.println("* Welcome to my shop! *");
+                        System.out.println("***********************");
+                        System.out.println("1 - Login");
+                        System.out.println("***********************");
+                        System.out.println("0 - Exit \n");
+                        System.out.print("What do you want to  do? : ");
         if (scanner.hasNextInt()) {
             int input = scanner.nextInt();
             switch (input) {
@@ -36,16 +42,107 @@ public class JavaActivityBootcamp {
                 System.out.print("Password: ");
                 String password = scanner.next();
                 user.setPassword(password);
-//                TextField password = new TextField(8);
-//                password.setEchoChar('*');
-                System.out.println(username);
+                users.add(user);
+                System.out.println("Please select user type. \n"
+                            + "0 - Admin \n" 
+                            + "1 - Customer \n");
+                Scanner scanner2 = new Scanner(System.in);
+                if (scanner2.hasNextInt()) {
+                    int loginput = scanner2.nextInt();
+                    switch (loginput) {
+                    case 0:
+                    System.out.println("Logged in as admin. \n");  
+                    System.out.println("***********************");
+                    System.out.println("*       ADMIN         *");
+                    System.out.println("***********************");
+                    System.out.println("1 - Manage products");
+                    System.out.println("2 - Manage orders");
+                    System.out.println("***********************");
+                    System.out.println("0 - Logout");
+                    Scanner admin1 = new Scanner(System.in);
+                        if (admin1.hasNextInt()) {
+                            int adminlog1 = admin1.nextInt();
+                            switch (adminlog1) {
+                                case 1:  
+                                System.out.println("***********************");
+                                System.out.println("*       PRODUCTS      *");
+                                System.out.println("***********************");
+                                System.out.println("<Product list here>");
+                                System.out.println("***********************");
+                                System.out.println("0 - Back");
+                                System.out.println("1 - Add new product");
+                                System.out.println("2 - Remove product");
+                                break;
+                                case 2:
+                                System.out.println("***********************");
+                                System.out.println("*       ORDERS      *");
+                                System.out.println("***********************");
+                                System.out.println("<Order list here>");
+                                System.out.println("***********************");                         
+                                System.out.println("1 - Mark order as delivered.");
+                                System.out.println("0 - Back");
+                                default: 
+                                System.out.println("***********************");
+                                System.out.println("*       ADMIN         *");
+                                System.out.println("***********************");
+                                System.out.println("1 - Manage products");
+                                System.out.println("2 - Manage orders");
+                                System.out.println("***********************");
+                                break;
+                            }
+                        }
+                        break;
+                    case 1:
+                    System.out.println("Logged in as customer. \n");    
+                    System.out.println("***********************");
+                    System.out.println("*       CUSTOMER      *");
+                    System.out.println("***********************");
+                    System.out.println("1 - Shop");
+                    System.out.println("2 - My Orders");
+                    System.out.println("***********************");
+                    System.out.println("0 - Logout");
+                    Scanner cust1 = new Scanner(System.in);
+                        if (cust1.hasNextInt()) {
+                            int custlog1 = cust1.nextInt();
+                            switch(custlog1) {
+                                case 1:
+                                System.out.println("***********************");
+                                System.out.println("*       SHOP      *");
+                                System.out.println("***********************");
+                                System.out.println("<Product list here>");
+                                System.out.println("***********************");
+                                System.out.print("0 - Back");
+                                case 2:
+                                System.out.println("***********************");
+                                System.out.println("*       MY ORDERS      *");
+                                System.out.println("***********************");
+                                System.out.println("<Order list here>");
+                                System.out.println("***********************");
+                                System.out.print("Press 'ENTER' to continue.");
+                                Scanner enter = new Scanner(System.in);
+                                enter.nextLine();
+                                break;
+                                default:    
+                                System.out.println("***********************");
+                                System.out.println("*       CUSTOMER      *");
+                                System.out.println("***********************");
+                                System.out.println("1 - Shop");
+                                System.out.println("2 - My Orders");
+                                System.out.println("***********************");
+                                System.out.println("0 - Logout");   
+                            }
+                        }
+                    }   
+                }
                 break;
              case 0: 
                  System.out.println("Logging out.");
+                 break;
              default:
                  System.out.println("Please enter only 0 or 1.");
+                 break;
         }
-    }
- }
-    
+     }
+   }      
 }
+    
